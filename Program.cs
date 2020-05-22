a89fa20dd14a522dba94094d8b9e2539fe4c4783
fabricio <fnmamao@gmail.com>
2020-05-22 10:33:31 -0300

    adiciona exercicio

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int C;
+            double f;
+            Console.WriteLine("Conversor de Celcius em Fahrenheit");
+            Console.WriteLine("informe a temperatura em celcius:");
+            C = Convert.ToInt16(Console.ReadLine());
+            
+            // conversão
+            f = C * 1.8 + 32;
+
+            Console.WriteLine("a temperatura em Fahrenheit é " + f);
+            
+        }
+    }
+}
